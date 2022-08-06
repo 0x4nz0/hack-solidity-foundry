964cb0dd7ff4fef55b13a700d898dcfced2ed182
0x4nz0 <4nz0@proton.me>
2022-08-06 14:27:43 +0200

    test: add test for AccessingPrivateData contract

---
+++ test/AccessingPrivateData.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/AccessingPrivateData.sol";
+
+contract AccessingPrivateDataTest is Test {
+    Vault public vault;
+    
+    function setUp() public {
+        vault = new Vault(bytes32("secret"));
+    }
+    
+    function testAccessingPrivateData() public {
+        bytes32 secret = vm.load(address(vault), bytes32(uint256(2)));
+        assertEq(bytes32("secret"), secret);
+    }
+}
