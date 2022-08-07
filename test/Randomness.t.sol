0aea3f3a656978dbcc4f5e17a4b554c107df73d1
0x4nz0 <4nz0@proton.me>
2022-08-07 16:01:02 +0200

    test: add test for Randomness contract

---
+++ test/Randomness.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Randomness.sol";
+
+contract RandomnessTest is Test {
+    GuessTheRandomNumber public guessTheRandomNumber;
+    
+    function setUp() public {
+        address alice = vm.addr(1);
+        hoax(alice);
+        guessTheRandomNumber = new GuessTheRandomNumber{value: 1 ether}();
+    }
+    
+    function testGuessTheRandomNumber() public {
+        assertEq(address(guessTheRandomNumber).balance, 1 ether);
+        
+        address eve = vm.addr(3);
+        startHoax(eve);
+
+        Attack attack = new Attack();
+        attack.attack(guessTheRandomNumber);
+
+        assertEq(address(guessTheRandomNumber).balance, 0 ether);
+        assertEq(address(attack).balance, 1 ether);
+    }
+}
